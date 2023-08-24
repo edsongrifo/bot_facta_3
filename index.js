a7bc59890dc8272af236de6f55717a28cfc6f5c4
unknown <edson.empreendedor.digital@gmail.com>
2023-08-24 16:09:10 -0300

    Novo3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
-const PORT = process.env.PORT || 4000; //teste
+const PORT = process.env.PORT || 3000;
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
